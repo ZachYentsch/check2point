d2051dbda146c6dffd900abe9df078d9f8f41d2e
ZachYentsch <z6chy@icloud.com>
2021-12-12 13:18:31 -0700

    push

--- app.js
+++ app.js
@@ -1,41 +1,63 @@
-let sales = 0;
+let ticker = 0;
+let clU = 1;
+let manUps = 0;
 
 let clickUpgrades = {
-    beats: {
+    mixup: {
         price: 50,
         quantity: 0,
         multiplier: 1
     }
 };
 let automaticUpgrades = {
-    vine: {
+    manager: {
         price: 100,
         quantity: 0,
         multiplier: 25
     }
 };
+
 function mine() {
     let template = ''
-    sales++;
-    template += `Sales: ${sales}`
-    document.getElementById('sa').innerText = template
-    if (sales >= 10) {
-        clickUpgrades.beats.quantity++
-        sales++
-        document.getElementById('beatz').style.display = "block"
-    }
-    else document.getElementById('beatz').style.display = "none"
+    ticker += clU
+    template += `${ticker}`
+    document.getElementById('counter').innerHTML = template
 }
 
 function clickUp() {
-    for (let i = `${sales}`; i < clickUpgrades.beats.quantity.length; i++) {
-        const key = clickUpgrades[i];
+    let template = ''
+    let template1 = ''
+    let template2 = ''
+    template += `${ticker}`
+    template1 += `${clU}`
+    template2 += `${clickUpgrades.mixup.price}`
+    if (ticker >= clickUpgrades.mixup.price) {
+        clU += 1
+        ticker -= clickUpgrades.mixup.price
+        clickUpgrades.mixup.price = Math.round(clickUpgrades.mixup.price * 1.25);
+        document.getElementById('counter').innerHTML = template
+        document.getElementById('mixUp').innerHTML = template1
+        document.getElementById('mxUpPrice').innerHTML = template2
     }
-    drawClickUp()
 }
 
-function drawClickUp() {
+function autoUp() {
     let template = ''
-    template += `Beats: ${clickUpgrades.beats.quantity}`
-    document.getElementById('Beatz').innerText = template
+    let template2 = ''
+    if (ticker >= automaticUpgrades.manager.price) {
+        manUps += 1;
+        ticker -= automaticUpgrades.manager.price;
+        template += `${manUps}`
+        template2 += `${automaticUpgrades.manager.price}`
+        document.getElementById('manUp').innerHTML = template
+        document.getElementById('autoManPrice').innerHTML = template2
+    }
+    startInterval()
+}
+
+function startInterval() {
+    setInterval(() => {
+        ticker += manUps
+    }, 3000)
+
 }
